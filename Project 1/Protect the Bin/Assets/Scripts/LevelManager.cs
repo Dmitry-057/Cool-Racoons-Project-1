aa64bb11d43864da43871b309420151fb2c2f38a
Dmitry-057 <merezhko.dmitry@gmail.com>
2020-02-23 16:48:15 -0800

    Modified script and sprites' pivot position so that tiles take up the whole screen.

--- Project 1/Protect the Bin/Assets/Scripts/LevelManager.cs
+++ Project 1/Protect the Bin/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,11 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject tile;
+
+    public float TileSize
+    {
+        get { return tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -22,15 +27,20 @@ public class LevelManager : MonoBehaviour
 
     private void CreateLevel()
     {
-        float tileWidth = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        Vector3 startPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
 
         for (int y = 0; y < 5; y++)
         {
             for (int x = 0; x < 9; x++)
             {
-                GameObject newTile = Instantiate(tile);
-                newTile.transform.position = new Vector3(tileWidth * x,tileWidth * y, 0);
+                PlaceTile(x, y, startPosition);
             }
         }
+    }
+
+    private void PlaceTile(int x, int y, Vector3 startPosition)
+    {
+        GameObject newTile = Instantiate(tile);
+        newTile.transform.position = new Vector3(startPosition.x +(TileSize * x), startPosition.y - (TileSize * y), 0);
     }
 }
