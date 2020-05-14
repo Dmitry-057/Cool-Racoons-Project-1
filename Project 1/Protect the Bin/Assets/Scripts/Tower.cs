c366cf6ea6cbdddfdb129aea07d3f18402e9f31a
Bridget <bridgetnaylor2000@gmail.com>
2020-05-14 08:16:55 -0700

    Finished 9.1, 9.2 -> Projectiles and projectile movement Skipped 9.3 and 9.4 because we don't need them Made Sprite background for Raccoon boss transparent.

--- Project 1/Protect the Bin/Assets/Scripts/Tower.cs
+++ Project 1/Protect the Bin/Assets/Scripts/Tower.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
 
+    [SerializeField]
+    private string projectileType;
+
+    [SerializeField]
+    private float projectileSpeed;
+
+    public float ProjectileSpeed
+    {
+        get { return projectileSpeed; }
+    }
 
     private SpriteRenderer mySpriteRenderer;
 
     private Monster target;
 
+    public Monster Target
+    {
+        get { return target; }
+    }
+
     private Queue<Monster> monsters = new Queue<Monster>();
+
+    private bool canAttack = true;
+
+    private float attackTimer;
+
+    [SerializeField]
+    private float attackCooldown;
 
 
     // Start is called before the first frame update
@@ -33,10 +55,40 @@ public class Tower : MonoBehaviour
 
     public void Attack()
     {
+        if ( !canAttack )
+        {
+            attackTimer += Time.deltaTime;
+
+
+            if ( attackTimer >= attackCooldown )
+            {
+                canAttack = true;
+                attackTimer = 0;
+            }
+        }
         if ( target == null && monsters.Count > 0 ) 
         {
             target = monsters.Dequeue();
         }
+        if ( target != null && target.IsActive )
+        {
+            if ( canAttack )
+            {
+                Shoot();
+
+                canAttack = false;
+            }
+            
+        }
+    }
+
+    private void Shoot()
+    {
+        Projectile projectile = GameManager.Instance.Pool.GetObject( projectileType ).GetComponent<Projectile>();
+
+        projectile.transform.position = transform.position;
+
+        projectile.Initialize(this);
     }
 
     public void OnTriggerEnter2D( Collider2D other )
